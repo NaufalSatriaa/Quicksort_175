2b4cbb2abbf953bedfe663103d8311b103cf3448
NaufalSatriaa <satriana0017@gmail.com>
2023-03-31 11:37:07 +0700

    membuat keterangan pada coding

--- Quicksort_Mergesort/Quicksort_Mergesort.cpp
+++ Quicksort_Mergesort/Quicksort_Mergesort.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 
-int arr[20]; 
-int cmp_count = 0;
-int mov_count = 0;
+int arr[20]; //array of integres to hold values
+int cmp_count = 0;// number of comparasion
+int mov_count = 0;// number of movement
 int n;
 
 void input() {
@@ -26,7 +26,7 @@ void input() {
 		cin >> arr[i];
 	}
 }
-
+// swap the element at index x with the element index y
 void swap(int x, int y) {
 	int temp;
 	temp = arr[x];
@@ -39,31 +39,40 @@ void q_sort(int low, int high) {
 	if (low > high)
 		return;
 
+	// partition the list into two parts
+	// one centaining element less that or equal to pivot
+	// outher containing element greather than pivot
 	i = low + 1;
 	j = high;
 	pivot = arr[low];
 
 	while (i <= j) {
+		//search for an element greater than pivot
 		while ((arr[i] <= pivot) && (i <= high)) {
 			i++;
 			cmp_count++;
 		}
 		cmp_count++;
+		///search for an element less thanor equal to pivot
 		while ((arr[j] > pivot) && (j >= low)) {
 			j--;
 			cmp_count++;
 		}
 	}
 	cmp_count++;
-	if (i < j) {
+	if (i < j) {//if greater element is on left of the element
+		
+		//swap the element at index i with the element at index j
 		swap(i, j);
 		mov_count++;
-	}
+	}//j now containt the index of the last element in the sort list
 	if (low < j) {
+		//move to the pivot to its correct position in the list
 		swap(low, j);
 		mov_count++;
-	}
+	}//sort the list on the left of pivot using quick sort
 	q_sort(low, j - 1);
+	//short the list on the right of pivot using quizl sort
 	q_sort(j + 1, high);
 }
 
